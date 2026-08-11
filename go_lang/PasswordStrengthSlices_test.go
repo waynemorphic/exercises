511882bbd53b69c12a85ff73588a369d9dbe2ba7
waynemorphic <wayneklyde@gmail.com>
2026-08-11 22:14:05 +0300

    testing: Test suites for slices for password strength

---
+++ go_lang/PasswordStrengthSlices_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsValidPassword(t *testing.T) {
+	type testCase struct {
+		password string
+		isValid  bool
+	}
+
+	runCases := []testCase{
+		{"Pass123", true},
+		{"pas", false},
+		{"Password", false},
+		{"123456", false},
+	}
+
+	submitCases := append(runCases, []testCase{
+		{"VeryLongPassword1", false},
+		{"Short", false},
+		{"1234short", false},
+		{"Short5", true},
+		{"P4ssword", true},
+		{"AA0Z9", true},
+	}...)
+
+	testCases := runCases
+
+	skipped := len(submitCases) - len(testCases)
+
+	passCount := 0
+	failCount := 0
+
+	for i, test := range testCases {
+		t.Run(fmt.Sprintf("TestCase%d", i+1), func(t *testing.T) {
+			result := isValidPassword(test.password)
+			if result != test.isValid {
+				failCount++
+				t.Errorf(`---------------------------------
+Password:  "%s"
+Expecting: %v
+Actual:    %v
+Fail
+`, test.password, test.isValid, result)
+			} else {
+				passCount++
+				fmt.Printf(`---------------------------------
+Password:  "%s"
+Expecting: %v
+Actual:    %v
+Pass
+`, test.password, test.isValid, result)
+			}
+		})
+	}
+
+	fmt.Println("---------------------------------")
+	if skipped > 0 {
+		fmt.Printf("%d passed, %d failed, %d skipped\n", passCount, failCount, skipped)
+	} else {
+		fmt.Printf("%d passed, %d failed\n", passCount, failCount)
+	}
+}
